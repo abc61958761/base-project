504f6bbb50b04f0af145cd5f565e9e5bedafd5b1
Carol.Li <abc61958761@gmail.com>
2022-05-15 18:36:41 +0800

    fix menus mobile page

--- components/Menus.vue
+++ components/Menus.vue
@@ -6,17 +6,114 @@
     </div>
     <div class="menu_content">
       <div class="menu_content_left">
-        <div>
-          <div @click="clickMenu(name)" v-for="(item, name, index) of menuItems" :key="index" class="menu_item">{{name}}</div>
+        <div  v-for="(item, name, index) of menuItems" :key="index" >
+          <div @click="clickMenu(item, name)" class="menu_item" :class="{ selected_menu_item: item.selected }">{{name}}</div>
+          <div @click="clickSubMenu(subItem, name)" v-for="(subItem, itemIndex) of item.items" :key="itemIndex" class="menu_subItem" :class="{ selected_menu_item: subItem.selected }" v-if="item.selected">
+            {{subItem.name}}
+          </div>
         </div>
       </div>
       <div class="menu_content_right">
-        <div v-for="(menu, index) of data" :key="index" class="menu_content_detail">
+        <div v-if="selectedMenu == 'FOOD'" v-for="(menu, index) of data" :key="index" class="menu_content_detail">
           <div class="menu_content_title" v-html="menu.title.toUpperCase()"></div>
-          <div style="display: flex; flex:1 1 auto; flex-wrap: wrap">
+          <div v-if="menu.title == 'something to share'"  class="FOOD_menu_content_SHARE">
+            <div>
+              <div v-for="(item, contentIndex) of menu.items.left" :key="contentIndex">
+                <div class="menu_item_title" v-html="item.title"></div>
+                <div v-if="item.content" class="menu_item_content" v-html="item.content"></div>
+              </div>
+            </div>
+            <div>
+              <div class="line_dotted">................</div>
+              <div v-for="(item, contentIndex) of menu.items.right" :key="contentIndex">
+                <div class="menu_item_title" v-html="item.title"></div>
+                <div v-if="item.content" class="menu_item_content" v-html="item.content"></div>
+              </div>
+              <div class="line_dotted">................</div>
+            </div>
+          </div>
+          <div v-else style="display: flex; flex:1 1 auto; flex-wrap: wrap">
             <div v-for="(item, contentIndex) of menu.items" :key="contentIndex" class="menu_detail">
               <div class="menu_item_title" v-html="item.title"></div>
-              <div class="menu_item_content" v-html="item.content"></div>
+              <div v-if="item.content" class="menu_item_content" v-html="item.content"></div>
+            </div>
+          </div>
+        </div>
+        
+        <div v-if="selectedMenu == 'DRINK'">
+          <div v-if="selectedSubMenu == 'ALCOHOLIC'">
+            <div v-for="(menu, index) of data" :key="index" class="menu_content_detail">
+              <div class="DRINK_menu_content_title">
+                <div class="menu_content_title" v-html="menu.title.toUpperCase()"></div>
+                <div class="menu_content_sub_title" v-html="menu.discript"></div>
+              </div>
+              <div v-if="menu.others" class="DRINK_menu_content_others">
+                <div>
+                  <div v-for="(item, contentIndex) of menu.others.left" :key="contentIndex">
+                    <div class="DRINK_menu_title">
+                      <div :class="`DRINK_menu_item_${item.type}`" v-html="item.title"></div>
+                      <div class="DRINK_menu_item_sub_title" v-html="item.subTitle"></div>
+                    </div>
+                    <div v-if="item.content" class="menu_item_content" :class="`DRINK_menu_item_content_${item.type}`" v-html="item.content"></div>
+                  </div>
+                </div>
+                <div>
+                  <div v-for="(item, contentIndex) of menu.others.right" :key="contentIndex">
+                    <div class="DRINK_menu_title">
+                      <div :class="`DRINK_menu_item_${item.type}`" v-html="item.title"></div>
+                      <div class="DRINK_menu_item_sub_title" v-html="item.subTitle"></div>
+                    </div>
+                    <div v-if="item.content" class="menu_item_content" :class="`DRINK_menu_item_content_${item.type}`" v-html="item.content"></div>
+                  </div>
+                </div>
+              </div>
+              <div style="display: flex; flex:1 1 auto; flex-wrap: wrap">
+                <div v-for="(item, contentIndex) of menu.items" :key="contentIndex" class="menu_detail">
+                  <div class="DRINK_menu_title" :class="`DRINK_menu_item_${menu.title.toUpperCase()}_${item.type}`">
+                    <div :class="`DRINK_menu_item_${item.type}`" v-html="item.title"></div>
+                    <div class="DRINK_menu_item_sub_title" v-html="item.subTitle"></div>
+                  </div>
+                  <div v-if="item.content" class="menu_item_content" :class="`DRINK_menu_item_content_${item.type}`" v-html="item.content"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div v-if="selectedSubMenu == 'NON-ALCOHOLIC'">
+            <div v-for="(menu, index) of data" :key="index" class="menu_content_detail">
+              <div class="DRINK_menu_content_title">
+                <div class="menu_content_title" v-html="menu.title.toUpperCase()"></div>
+                <div class="menu_content_sub_title" v-html="menu.discript"></div>
+              </div>
+              <div style="display: flex; flex:1 1 auto; flex-wrap: wrap">
+                <div v-for="(item, contentIndex) of menu.items" :key="contentIndex" class="menu_detail">
+                  <div class="DRINK_menu_item_item" v-html="item.title"></div>
+                  <div class="menu_item_content" v-html="item.content"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="selectedMenu == 'SHISHA'" >
+          <div class="menu_content_detail">
+          <div style="margin-bottom: 20px;">
+            <div class="SHISHA_title" v-html="data.title.toUpperCase()"></div>
+          </div>
+          <div style="display: flex; flex:1 1 auto; flex-wrap: wrap">
+            <div v-for="(item, contentIndex) of data.items" :key="contentIndex" class="menu_detail">
+              <div class="menu_item_SHISHA_title" v-html="item.title.toUpperCase()"></div>
+              <div class="menu_item_SHISHA_content" v-html="item.content"></div>
+            </div>
+          </div>
+            <div class="SHISHA_other">
+              <img src="~/assets/images/Asset 14.svg" style="position: absolute;" />
+              <ShishaText class="SHISHA_other_text" />
+              <div class="SHISHA_other_content"> 
+                <div class="SHISHA_other_sub_title" v-html="data.other.subTitle"></div>
+                <div class="SHISHA_other_material" v-html="data.other.material"></div>
+                <div class="SHISHA_other_guzzle" v-html="data.other.guzzle"></div>
+              </div>
             </div>
           </div>
         </div>
@@ -26,19 +123,62 @@
 </template>
 
 <script>
-import menuData from '~/assets/data/menu.json'
+import menuData from '~/assets/data/menuData.json'
 export default {
   name: 'Menus',
   data: () => {
       return {
-        menuItems: menuData,
-        data: menuData.FOOD
+        menuItems: {
+          "FOOD": {
+            items: [],
+            selected: true
+          },
+          "DRINK": {
+            items: [{
+              name: "ALCOHOLIC",
+              selected: false
+            }, {
+              name:  "NON-ALCOHOLIC",
+              selected: false
+            }],
+            selected: false
+          }, 
+          "SHISHA": {
+            items: [],
+            selected: false
+          },
+        },
+        data: menuData.FOOD,
+        selectedMenu: "FOOD",
+        selectedSubMenu: null
     }
   },
   methods: {
-    clickMenu (item) {
-      console.log(item)
-      this.data = menuData[item]
+    clickMenu (item, name) {
+      this.selectedMenu = name
+      this.data = menuData[name];
+      for(const [key, value] of Object.entries(this.menuItems)) {
+        value.selected = false;
+      }
+      item.selected = true;
+
+      if (item.items.length > 0) {
+        item.items.forEach(data => {
+          data.selected = false;
+        })
+        item.items[0].selected = true;
+        this.data = this.data[item.items[0].name]
+        this.selectedSubMenu = item.items[0].name;
+      }
+    }, 
+    clickSubMenu (item, key) {
+      const menuItem = this.menuItems[key]
+      menuItem.items.forEach(data => {
+        data.selected = false;
+      })
+      item.selected = true;
+      this.selectedSubMenu = item.name;
+      this.data = menuData[key][item.name]
     }
   }
 }
@@ -50,9 +190,7 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  /* padding: 0 175px; */
   flex: 1;
-  /* max-width: 1024px; */
   align-items: center;
 }
 .menu_title {
@@ -81,21 +219,33 @@ export default {
   align-items: center;
   margin-bottom: 30px;
   cursor: pointer;
+  opacity: 0.5;
 }
 .menu_content {
   display: flex;
   max-width: 1440px;
   font-size: 21px;
   font-family: 'JMH';
+  width: 100%;
+  padding-bottom:130px;
 }
 .menu_item_content {
   color: #455336;
   font-size: 15px;
+  margin-bottom: 40px;
 }
 .menu_content_title {
   color: #F36F43;
-  font-family: Tempestua;
+  font-family: "Tempestua";
   font-size: 68px;
+  line-height: 60px;
+  margin-bottom: 20px;
+}
+.menu_content_sub_title {
+  color: #392417;
+  font-size: 21px;
+  font-family: 'JMHBold';
+  margin-left: 16px;
 }
 .menu_content_right {
   height: 700px;
@@ -115,11 +265,127 @@ export default {
 .menu_detail {
   max-width:50%;
   flex: 0 0 50%;
-  margin-bottom: 40px;
   padding: 4px;
 }
 .menu_content_detail {
   margin-bottom: 40px
+}
+.menu_subItem {
+  border-left:5px solid #455336;
+  margin-bottom: 28px;
+  padding-left: 28px;
+  line-height: 28px;
+  cursor: pointer;
+  opacity: 0.5;
+}
+.selected_menu_item {
+  opacity: 1;
+}
+.SHISHA_title {
+  display: inline;
+  color: #3A2416;
+  font-size: 48px;
+  line-height: 41px;
+  margin-bottom: 16px;
+  background: radial-gradient(circle at 10px -7px, transparent 8px, #F36F43 8px, #3A2416 9px, transparent 9px) repeat-x,
+        radial-gradient(circle at 10px 27px, transparent 8px, #F36F43 8px, #F36F43 9px, transparent 9px) repeat-x;
+  background-size: 20px 20px;
+  background-position: -10px calc(100% + 16px), 0 calc(100% - 4px);
+}
+.SHISHA_other_content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 90px;
+}
+.menu_item_SHISHA_title {
+  font-size: 23px;
+  color: #F36F43;
+  /* font-family: JMHBold; */
+}
+.menu_item_SHISHA_content {
+  color: #455235;
+  font-size: 15px;
+  font-family: 'JMH';
+  margin-bottom: 16px;
+}
+.SHISHA_other {
+  width: 850px;
+  height: 380px;
+  background-repeat: no-repeat;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 16px;
+}
+.SHISHA_other_text {
+  top: -80px;
+  position: absolute;
+}
+.SHISHA_other_sub_title {
+  margin-bottom: 40px;
+  font-family: 'JMHBold';
+  font-size: 28px;
+}
+.SHISHA_other_material {
+  font-family: 'JMHBold';
+  font-size: 21px;
+  margin-bottom: 20px;
+}
+.SHISHA_other_guzzle {
+  font-family: 'JMH';
+  font-size: 16px;
+}
+.DRINK_menu_item_title {
+  color: #F36F43;
+  font-size: 23px;
+  font-family: 'JMH';
+}
+.DRINK_menu_item_item {
+  color: #392417;
+  font-size: 21px;
+  font-family: 'JMHBold';
+}
+.DRINK_menu_item_sub_title {
+  margin-left: 8px;
+  color: #392417;
+  font-size: 15px;
+}
+.DRINK_menu_item_content_title {
+  margin-bottom: 20px;
+}
+.DRINK_menu_item_content_item {
+  margin-bottom: 40px;
+}
+.DRINK_menu_item_SPIRITS_item {
+  margin-bottom: 40px;
+}
+.FOOD_menu_content_SHARE {
+  display: flex; 
+  flex:1 1 auto;
+}
+.FOOD_menu_content_SHARE > div {
+  width: 50%;
+}
+.DRINK_menu_content_others {
+  display: flex; 
+  flex:1 1 auto;
+}
+.DRINK_menu_content_others > div{
+  width: 50%;
+}
+.line_dotted {
+  letter-spacing: 10px;
+  line-height: 30px; 
+  font-weight: bold; 
+  color: #F36F43;
+  margin-bottom: 40px;
+}
+.DRINK_menu_title {
+  display: flex; 
+  align-items: center;
 }
 @media screen and (max-width: 600px) {
   .menu {
@@ -136,6 +402,7 @@ export default {
   .menu_content {
     flex-direction: column;
     width: 100%;
+    padding-bottom: 50px;
   }
   .menu_content_right {
     width: 100%;
@@ -144,15 +411,10 @@ export default {
     width: 100%;
     overflow: scroll;
     margin-bottom: 20px;
+    flex-direction: row;
   }
   .menu_content_left::-webkit-scrollbar {
     display:none
-  }
-  .menu_content_left > div {
-    display: flex;
-    flex-direction: row;
-    width: max-content;
-    flex: 1;
   }
   .menu_item {
     width: 120px;
@@ -169,14 +431,64 @@ export default {
   }
   .menu_item_content {
     font-size: 12px;
+    margin-bottom: 20px;
   }
   .menu_detail {
     max-width:100%;
     flex: 0 0 100%;
-    margin-bottom: 20px;
   }
   .menu_content_detail {
     margin-bottom: 28px
+  }
+  .FOOD_menu_content_SHARE {
+    flex-direction: column;
+  }
+  .FOOD_menu_content_SHARE > div {
+    width: 100%;
+  }
+  .menu_subItem {
+    font-size: 14px;
+    margin-bottom: 8px;
+    line-height: 20px;
+    padding-left: 8px;
+  }
+  .DRINK_menu_content_others {
+    flex-direction: column;
+  }
+  .DRINK_menu_content_others > div{
+    width: 100%;
+  }
+  .line_dotted {
+    margin-bottom: 20px;
+  }
+  .DRINK_menu_item_item {
+    font-size: 16px;
+  }
+  .DRINK_menu_item_title {
+    font-size: 18px;
+  }
+  .DRINK_menu_content_title > .menu_content_title {
+    margin-bottom: 0px;
+  }
+  .DRINK_menu_content_title > .menu_content_sub_title {
+    margin-left: 0px;
+    font-size: 18px;
+  }
+  .DRINK_menu_item_SPIRITS_item {
+    margin-bottom: 20px;
+  }
+  .SHISHA_title {
+    font-size: 30px;
+  }
+  .menu_item_SHISHA_title {
+    font-size: 18px;
+  }
+  .menu_item_SHISHA_content {
+    font-size: 14px;
+  }
+  .SHISHA_other {
+    transform: scale(0.4, 0.4);
+    transform-origin: top left;
   }
 }
 </style>
---
+++ components/ShishaText.vue
@@ -0,0 +1,117 @@
+<template>
+    <div id="warped">
+        <span class="w0">P</span><span class="w1">I</span><span class="w2">C</span><span class="w3">K</span><span class="w4" /><span class="w5">Y</span><span class="w6">O</span><span class="w7">U</span><span class="w8">R</span><span class="w9" /><span class="w10">O</span><span class="w11">W</span><span class="w12">N</span><span class="w13" /><span class="w14">F</span><span class="w15">L</span><span class="w16">A</span><span class="w17">V</span><span class="w18">O</span><span class="w19">U</span><span class="w20">R</span>
+    </div>
+</template>
+<style>
+#warped {
+    position: relative; 
+    display: block; 
+    width:588px; 
+    height:440px;
+    color: #F36F43;
+    font-weight: bold;
+}
+
+#warped>span[class^=w]:nth-of-type(n+0){display:block; position:absolute;
+-moz-transform-origin:50% 100%; -webkit-transform-origin:50% 100%; -o-transform-origin:50%
+100%; -ms-transform-origin:50% 100%; transform-origin:50% 100%; }
+
+#warped span{font-family:'ABeeZee';font-size:38px;font-weight:regular;font-style:normal;
+line-height:0.65; white-space:pre; overflow:visible; padding:0px;}
+
+#warped .w0 {-moz-transform: rotate(-7.19rad);-webkit-transform: rotate(-7.19rad);-o-transform:
+rotate(-7.19rad);-ms-transform: rotate(-7.19rad); transform: rotate(-7.19rad);
+width: 22px; height: 25px; left: 26.56px; top: 188.28px;}
+
+#warped .w1 {-moz-transform: rotate(-7.02rad);-webkit-transform: rotate(-7.02rad);-o-transform:
+rotate(-7.02rad);-ms-transform: rotate(-7.02rad); transform: rotate(-7.02rad);
+width: 10px; height: 25px; left: 46.5px; top: 173.49px;}
+
+#warped .w2 {-moz-transform: rotate(-6.88rad);-webkit-transform: rotate(-6.88rad);-o-transform:
+rotate(-6.88rad);-ms-transform: rotate(-6.88rad); transform: rotate(-6.88rad);
+width: 23px; height: 25px; left: 56.81px; top: 160.36px;}
+
+#warped .w3 {-moz-transform: rotate(-6.74rad);-webkit-transform: rotate(-6.74rad);-o-transform:
+rotate(-6.74rad);-ms-transform: rotate(-6.74rad); transform: rotate(-6.74rad);
+width: 23px; height: 25px; left: 81.84px; top: 145.99px;}
+
+#warped .w4 {-moz-transform: rotate(-6.66rad);-webkit-transform: rotate(-6.66rad);-o-transform:
+rotate(-6.66rad);-ms-transform: rotate(-6.66rad); transform: rotate(-6.66rad);
+width: 11px; height: 25px; left: 107.91px; top: 137.29px;}
+
+#warped .w5 {-moz-transform: rotate(-6.59rad);-webkit-transform: rotate(-6.59rad);-o-transform:
+rotate(-6.59rad);-ms-transform: rotate(-6.59rad); transform: rotate(-6.59rad);
+width: 23px; height: 25px; left: 122.43px; top: 130.21px;}
+
+#warped .w6 {-moz-transform: rotate(-6.51rad);-webkit-transform: rotate(-6.51rad);-o-transform:
+rotate(-6.51rad);-ms-transform: rotate(-6.51rad); transform: rotate(-6.51rad);
+width: 29px; height: 25px; left: 149.44px; top: 122.34px;}
+
+#warped .w7 {-moz-transform: rotate(-6.44rad);-webkit-transform: rotate(-6.44rad);-o-transform:
+rotate(-6.44rad);-ms-transform: rotate(-6.44rad); transform: rotate(-6.44rad);
+width: 25px; height: 25px; left: 183.23px; top: 116.52px;}
+
+#warped .w8 {-moz-transform: rotate(-6.38rad);-webkit-transform: rotate(-6.38rad);-o-transform:
+rotate(-6.38rad);-ms-transform: rotate(-6.38rad); transform: rotate(-6.38rad);
+width: 23px; height: 25px; left: 212.14px; top: 113.15px;}
+
+#warped .w9 {-moz-transform: rotate(-6.34rad);-webkit-transform: rotate(-6.34rad);-o-transform:
+rotate(-6.34rad);-ms-transform: rotate(-6.34rad); transform: rotate(-6.34rad);
+width: 11px; height: 25px; left: 240.15px; top: 111.5px;}
+
+#warped .w10 {-moz-transform: rotate(-6.3rad);-webkit-transform: rotate(-6.3rad);-o-transform:
+rotate(-6.3rad);-ms-transform: rotate(-6.3rad); transform: rotate(-6.3rad);
+width: 29px; height: 25px; left: 256.01px; top: 110.62px;}
+
+#warped .w11 {-moz-transform: rotate(-6.24rad);-webkit-transform: rotate(-6.24rad);-o-transform:
+rotate(-6.24rad);-ms-transform: rotate(-6.24rad); transform: rotate(-6.24rad);
+width: 36px; height: 25px; left: 290.55px; top: 111.18px;}
+
+#warped .w12 {-moz-transform: rotate(-6.19rad);-webkit-transform: rotate(-6.19rad);-o-transform:
+rotate(-6.19rad);-ms-transform: rotate(-6.19rad); transform: rotate(-6.19rad);
+width: 27px; height: 25px; left: 331.33px; top: 113.83px;}
+
+#warped .w13 {-moz-transform: rotate(-6.15rad);-webkit-transform: rotate(-6.15rad);-o-transform:
+rotate(-6.15rad);-ms-transform: rotate(-6.15rad); transform: rotate(-6.15rad);
+width: 11px; height: 25px; left: 363.37px; top: 116.78px;}
+
+#warped .w14 {-moz-transform: rotate(-6.11rad);-webkit-transform: rotate(-6.11rad);-o-transform:
+rotate(-6.11rad);-ms-transform: rotate(-6.11rad); transform: rotate(-6.11rad);
+width: 20px; height: 25px; left: 378.58px; top: 119.95px;}
+
+#warped .w15 {-moz-transform: rotate(-6.06rad);-webkit-transform: rotate(-6.06rad);-o-transform:
+rotate(-6.06rad);-ms-transform: rotate(-6.06rad); transform: rotate(-6.06rad);
+width: 19px; height: 25px; left: 404.02px; top: 125.07px;}
+
+#warped .w16 {-moz-transform: rotate(-6rad);-webkit-transform: rotate(-6rad);-o-transform:
+rotate(-6rad);-ms-transform: rotate(-6rad); transform: rotate(-6rad); width:
+25px; height: 25px; left: 426.82px; top: 131.86px;}
+
+#warped .w17 {-moz-transform: rotate(-5.91rad);-webkit-transform: rotate(-5.91rad);-o-transform:
+rotate(-5.91rad);-ms-transform: rotate(-5.91rad); transform: rotate(-5.91rad);
+width: 24px; height: 25px; left: 455.14px; top: 141.38px;}
+
+#warped .w18 {-moz-transform: rotate(-5.79rad);-webkit-transform: rotate(-5.79rad);-o-transform:
+rotate(-5.79rad);-ms-transform: rotate(-5.79rad); transform: rotate(-5.79rad);
+width: 29px; height: 25px; left: 481.43px; top: 154.72px;}
+
+#warped .w19 {-moz-transform: rotate(-5.58rad);-webkit-transform: rotate(-5.58rad);-o-transform:
+rotate(-5.58rad);-ms-transform: rotate(-5.58rad); transform: rotate(-5.58rad);
+width: 25px; height: 25px; left: 509.28px; top: 172.28px;}
+
+#warped .w20 {-moz-transform: rotate(-5.24rad);-webkit-transform: rotate(-5.24rad);-o-transform:
+rotate(-5.24rad);-ms-transform: rotate(-5.24rad); transform: rotate(-5.24rad);
+width: 23px; height: 25px; left: 529px; top: 194.16px;}
+
+@media screen and (max-width: 600px) {
+    #warped span {
+        font-size: 14px;
+    }
+}
+</style>
+<script>
+export default {
+    name: 'ShishaText'
+}
+</script>
